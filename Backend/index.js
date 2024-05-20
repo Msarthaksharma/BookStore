a3d0d9e88bd8fcdc149c43741704e8045c5c942b
Msarthaksharma <sarthaks.2011@gmail.com>
2024-05-20 03:30:21 -0400

    changes

--- Backend/index.js
+++ Backend/index.js
@@ -7,8 +7,8 @@ app.use(express.json())
 app.use(cors())
 
 app.use('/book',Book)
-console.log(process.env.PORT)
+console.log(process.env.PORT || 5500)
 
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT || 5500,()=>{
     console.log(`Server running on ${process.env.PORT}`)
 })
